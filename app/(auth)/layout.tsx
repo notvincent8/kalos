604d5b772672aa05225a32bb6b2f352048d7c451
Vincent <vincent.vdt@proton.me>
2026-02-18 19:06:24 +0100

    feat(layout): redirect authenticated users to dashboard

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,20 @@
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 import type { ReactNode } from "react"
+import { auth } from "@/server/auth"
 
 type LayoutProps = {
   children: ReactNode
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = async ({ children }: LayoutProps) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  if (session) {
+    redirect("/dashboard")
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-6 bg-background px-4 py-8">
       {children}
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,16 +1,6 @@
-import { headers } from "next/headers"
-import { redirect } from "next/navigation"
 import RegisterForm from "@/app/(auth)/register/RegisterForm"
-import { auth } from "@/server/auth"
 
 const Page = async () => {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
-  if (session) {
-    redirect("/dashboard")
-  }
-
   return (
     <div className="flex gap-8 flex-col w-fit rounded-lg border bg-card p-16 shadow-sm">
       <h1 className="scroll-m-20 text-left text-4xl font-bold tracking-tight text-balance">Get started</h1>
