6e7a9254606b3b52d01052640f584b0cc700ce6c
Vincent <vincent.vdt@proton.me>
2026-02-21 17:50:54 +0100

    feat(header): wrap site title in a link for navigation to home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -23,7 +23,9 @@ const Header = () => {
 
   return (
     <header className="bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100 flex items-center justify-between px-4 py-2">
-      <h1 className="text-2xl uppercase font-bold">Kalos</h1>
+      <Link href="/">
+        <span className="text-2xl uppercase font-bold">Kalos</span>
+      </Link>
       {!isAuthenticated ? (
         <div className="flex flex-row gap-5">
           <Button asChild variant="link">
