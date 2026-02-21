8ee1601ecda3cd8859ac20ce6996d3f44e5eeb6e
Vincent <vincent.vdt@proton.me>
2026-02-21 01:41:37 +0100

    feat(auth): add registration and login links to authentication pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import LoginForm from "@/app/(auth)/login/LoginForm"
 
 const Page = () => {
@@ -5,6 +6,12 @@ const Page = () => {
     <div className="flex gap-8 flex-col w-fit rounded-lg border bg-card p-16 shadow-sm">
       <h1 className="scroll-m-20 text-left text-4xl font-bold tracking-tight text-balance">Welcome back</h1>
       <LoginForm />
+      <span>
+        You don't have an account?
+        <Link href="/register" className="ml-1 text-sm text-primary hover:underline">
+          Register
+        </Link>
+      </span>
     </div>
   )
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import RegisterForm from "@/app/(auth)/register/RegisterForm"
 
 const Page = async () => {
@@ -5,6 +6,12 @@ const Page = async () => {
     <div className="flex gap-8 flex-col w-fit rounded-lg border bg-card p-16 shadow-sm">
       <h1 className="scroll-m-20 text-left text-4xl font-bold tracking-tight text-balance">Get started</h1>
       <RegisterForm />
+      <span>
+        Already have an account?
+        <Link href="/login" className="ml-1 text-sm text-primary hover:underline">
+          Log in
+        </Link>
+      </span>
     </div>
   )
 }
